a82ce1c47dc675ea9b9ebad91f10dd1111d6d6d4
LotoInvestmentCo <bargi123456@gmail.com>
2025-01-08 19:20:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -95,6 +95,9 @@ def logout():
     session.pop('admin', None)
     return redirect(url_for('login'))
 
-if __name__ == '__main__':
+import os
+
+if __name__ == "__main__":
     init_db()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
